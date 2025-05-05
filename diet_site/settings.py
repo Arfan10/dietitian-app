3945a4bc94f65c63bcfa70040aa235cce3454163
Arfan10 <ayanshaikh0852@gmail.com>
2025-05-05 10:27:04 +0530

    Staticfiles created

--- diet_site/settings.py
+++ diet_site/settings.py
@@ -87,12 +87,21 @@ WSGI_APPLICATION = 'diet_site.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        
+    }
+}
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.parse(
+        DATABASE_URL,
         conn_max_age=600,
         ssl_require=True
     )
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -132,6 +141,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
   BASE_DIR / "theme" / "static",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
